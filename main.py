8ba0e1b5c730a874d2b1d05337b5c86bd984fa7b
Incognito <stephan.z@mail.ru>
2023-12-12 02:34:10 +0300

    Tags added

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from typing import Annotated, List
 app = FastAPI()
 
 
-@app.post('/token', response_model=schemas.Token)
+@app.post('/token', response_model=schemas.Token, tags=['token'])
 async def create_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
                        data_service: Annotated[services.Data, Depends(dependencies.get_data_service)],
                        token_service: Annotated[services.Token, Depends(dependencies.get_token_service)]):
@@ -27,12 +27,12 @@ async def create_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()
     return schemas.Token(access_token=token, token_type='bearer')
 
 
-@app.get('/user', response_model=schemas.User)
+@app.get('/user', response_model=schemas.User, tags=['user'])
 async def read_user(current_user: Annotated[models.User, Depends(dependencies.get_current_user)]):
     return current_user
 
 
-@app.put('/user/password', response_model=schemas.Response)
+@app.put('/user/password', response_model=schemas.Response, tags=['user'])
 async def update_password(current_password: Annotated[str, Form()],
                           new_password: Annotated[str, Form()],
                           current_user: Annotated[models.User, Depends(dependencies.get_current_user)],
@@ -48,7 +48,7 @@ async def update_password(current_password: Annotated[str, Form()],
     return schemas.Response(message='Password updated')
 
 
-@app.put('/user/username', response_model=schemas.Response)
+@app.put('/user/username', response_model=schemas.Response, tags=['user'])
 async def update_username(new_username: Annotated[str, Form()],
                           current_user: Annotated[models.User, Depends(dependencies.get_current_user)],
                           lock_service: Annotated[services.Lock, Depends(dependencies.get_lock_service)],
@@ -65,7 +65,7 @@ async def update_username(new_username: Annotated[str, Form()],
     return schemas.Response(message='Username updated')
 
 
-@app.get('/orders', response_model=List[schemas.Order])
+@app.get('/orders', response_model=List[schemas.Order], tags=['orders'])
 async def read_orders(current_user: Annotated[models.User, Depends(dependencies.get_current_user)],
                       data_service: Annotated[services.Data, Depends(dependencies.get_data_service)]):
     return await data_service.get_user_orders(current_user)
